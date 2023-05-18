bc5a2fc8a98ebf71e753d5eff0e692149831447f
Sadisu Rabiu Khamis <khaleepha2010@gmail.com>
2023-05-18 12:32:12 +0100

    update

---
+++ problem1_2.py
@@ -0,0 +1,6 @@
+def problem1_2(x,y):
+    sum = x + y
+    product = x * y
+    return print(sum,"\n",product)
+
+problem1_2(3,5)
---
+++ problem1_4.py
@@ -0,0 +1,5 @@
+def problem1_4(miles):
+    feet = 5280*miles
+    return print("there are ",feet," feet in ", miles, " miles")
+
+problem1_4(5)
